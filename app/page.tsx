9460e97ec7f66669591316545acdc6ea749de044
jrmesquta7 <cleobetojunior1999@gmail.com>
2025-11-21 06:41:53 -0300

    wip: adicionando design no fundo do card

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <main className="h-screen text-2">
+    <main className="min-h-screen text-2">
+
       <header className="flex items-center justify-between px-32 py-6">
         <Image
           src="/logo.png"
@@ -10,7 +11,6 @@ export default function HomePage() {
           width={117}
           height={51}
         />
-
         <nav className="flex gap-8">
           <span>Home</span>
           <span>Editais</span>
@@ -20,48 +20,79 @@ export default function HomePage() {
       </header>
 
 
-      <section>
-        aqui vai o banner
-      </section>
+      <section> aqui vai o banner </section>
 
-      <div className="max-w-7xl mx-auto flex flex-col">
+      <div className="max-w-7xl mx-auto flex flex-col items-center">
 
         <section className="pt-16 pb-8 flex flex-col items-center px-4">
           <h2 className="text-3xl md:text-5xl font-semibold text-center mb-4 text-[#333333] tracking-tight">
             O Problema que resolvemos
           </h2>
-
           <p className="text-base md:text-lg text-center max-w-lg mx-auto text-[#5C5C5C] leading-relaxed">
-            Hoje no Brasil, milhões de jovens enfrentam desafios para se
-            inserir no mercado de trabalho
+            Hoje no Brasil, milhões de jovens enfrentam desafios para se inserir no mercado de trabalho
           </p>
         </section>
 
-        <section className="w-full max-w-7xl mx-auto px-1 py-4">
-          <div className="grid grid-cols-10 gap-[16px] text-left">
-            <div className="col-span-4 bg-[#FFE5A1] rounded-[27px] p-8 flex flex-col justify-center relative overflow-hidden h-[180px]">
-              <h3 className="text-[#00963F] text-4xl md:text-5xl font-semi-bold leading-none mb-2 tracking-tight">
+        <section className="w-full flex justify-center gap-[16px] mb-2">
+
+          <div className="bg-[#FFE5A1] rounded-[27px] p-8 flex flex-col justify-center relative overflow-hidden w-[417px] h-[185px]">
+            <div className="absolute top-0 left-[-2%] top-[10%] w-[430px] h-[120px] z-0 pointer-events-none">
+              <Image
+                src="/Vector1.svg"
+                alt="Elemento decorativo"
+                width={430}
+                height={120}
+                className="object-none"
+                style={{ left: '-20px' }}
+              />
+            </div>
+            <div className="relative z-10">
+              <h3 className="text-[#00963F] text-4xl md:text-5xl font-semibold leading-none mb-2 tracking-tight">
                 +9<br />milhões
               </h3>
               <p className="text-[#4A4A4A] text-lg md:text-xl leading-tight font-medium">
                 de jovens não se inserem no mercado de trabalho.
               </p>
             </div>
+          </div>
 
-            <div className="col-span-2 bg-[#9BE5AA] rounded-[27px] flex items-center justify-center relative h-[180px]">
-              <div className="absolute left-[43%] -translate-x-1/2 -top-6 z-10 w-[245px] h-[245px]">
-                <Image
-                  src="/Maskgroup.svg"
-                  alt="Jovem estudante preocupado"
-                  width={600}
-                  height={600}
-                  className="object-contain"
-                />
-              </div>
+
+          <div className="bg-[#9BE5AA] rounded-[27px] flex items-center justify-center relative w-[270px] h-[185px]">
+
+
+            <div className="absolute top-[1%] left-[-16%] w-[350px] h-[179px] z-0 pointer-events-none">
+              <Image
+                src="/Vector2.svg"
+                alt="Elemento decorativo"
+                width={430}
+                height={120}
+                className="w-full h-full object-contain"
+              />
             </div>
-  
-            <div className="col-span-4 bg-[#C6C2F7] rounded-[27px] p-8 flex flex-col justify-center relative overflow-hidden h-[180px]">
-              <h3 className="text-[#5A4CA0] text-5xl md:text-6xl font-semi-bold leading-tight mb-2 tracking-tight">
+
+            <div className="absolute left-[45%] -translate-x-1/2 -top-13.5 z-10 w-[260px] h-[260px]">
+              <Image
+                src="/Maskgroup.svg"
+                alt="Jovem estudante preocupado"
+                fill
+                className="object-contain"
+              />
+            </div>
+          </div>
+
+          <div className="bg-[#C6C2F7] rounded-[27px] p-8 flex flex-col justify-center relative overflow-hidden w-[466px] h-[185px]">
+            <div className="absolute top-[0%] left-[-4%] w-[490px] h-[190px] z-0 pointer-events-none">
+              <Image
+                src="/Vector3.svg"
+                alt="Elemento decorativo"
+                width={425}
+                height={115}
+                className="w-full h-full object-contain"
+              />
+            </div>
+
+            <div className="relative z-10">
+              <h3 className="text-[#5A4CA0] text-5xl md:text-6xl font-semibold leading-tight mb-2 tracking-tight">
                 92.9%
               </h3>
               <p className="text-[#4A4A4A] text-lg md:text-xl leading-tight font-medium">
@@ -69,32 +100,59 @@ export default function HomePage() {
               </p>
             </div>
           </div>
+
+
         </section>
 
-        <section className="w-full max-w-7xl mx-auto px-1 py-0">
-          <div className="grid grid-cols-10 gap-[16px] text-left">
-            <div className="col-span-3 bg-[#FFE5A1] rounded-[27px] p-8 flex flex-col justify-center relative overflow-hidden h-[260px]">
-              <h3 className="text-[#00963F] text-4xl md:text-5xl font-semi-bold leading-none mb-2 tracking-tight">
-                75%<br />
+        <section className="w-full flex justify-center gap-[16px] mt-2">
+
+          <div className="bg-[#9BE5AA] rounded-[27px] p-8 flex flex-col justify-center relative overflow-hidden w-[339px] h-[265px]">
+
+            <div className="absolute top-[2%] -right-[15%] w-[460px] h-[267px] z-0 pointer-events-none opacity-60">
+              <Image
+                src="/Vector4.svg"
+                alt="Elemento decorativo"
+                width={400}
+                height={200}
+                className="w-full h-full object-contain"
+              />
+            </div>
+
+            <div className="relative z-10">
+              <h3 className="text-[#5A4CA0] text-4xl md:text-5xl font-semibold leading-none mb-2 tracking-tight">
+                75%
               </h3>
               <p className="text-[#4A4A4A] text-lg md:text-xl leading-tight font-medium">
                 identificam a desconexão entre o mercado e educação.
               </p>
             </div>
 
-            <div className="col-span-4 bg-[#9BE5AA] rounded-[27px] flex items-center justify-center relative h-[260px]">
-              <div className="absolute left-[43%] -translate-x-1/2 -top-6 z-10 w-[245px] h-[245px]">
-              </div>
+          </div>
+
+          <div className="bg-[#98CFFA] rounded-[27px] flex items-center justify-center relative overflow-hidden w-[441px] h-[265px]">
+          </div>
+
+          <div className="bg-[#FFE5A1] rounded-[27px] p-8 flex flex-col justify-center relative overflow-hidden w-[373px] h-[265px]">
+
+            <div className="absolute bottom-[20%] -right-[15%] w-[460px]  h-[230px] z-0 pointer-events-none opacity-80">
+              <Image
+                src="/Vector5.svg" 
+                alt="Elemento decorativo"
+                width={450}
+                height={200}
+                className="w-full h-full object-contain"
+              />
             </div>
-  
-            <div className="col-span-3 bg-[#C6C2F7] rounded-[27px] p-8 flex flex-col justify-center relative overflow-hidden h-[260px]">
-              <h3 className="text-[#5A4CA0] text-5xl md:text-6xl font-semi-bold leading-tight mb-2 tracking-tight">
+
+            <div className="relative z-10">
+              <h3 className="text-[#5A4CA0] text-5xl md:text-6xl font-semibold leading-tight mb-2 tracking-tight">
                 80%
               </h3>
               <p className="text-[#4A4A4A] text-lg md:text-xl leading-tight font-medium">
                 não tem garantia de formação que proporcione oportunidades de trabalho.
               </p>
             </div>
+
           </div>
         </section>
 

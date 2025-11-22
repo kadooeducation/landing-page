8f2817a22559c55402a789d3bcc86595146de3a1
Arthur Mesquita <arthurmesquita42@icloud.com>
2025-11-22 15:25:57 -0300

    feat: colocado banner como blackground e criado hero da página

--- app/page.tsx
+++ app/page.tsx
@@ -18,11 +18,11 @@ export default function HomePage() {
           <span>Contatos</span>
         </nav>
       </header>
-
+      
+      {/*
       <section className="relative overflow-hidden bg-gradient-to-r from-[#F1467D] to-[#FEB1CA] text-white">
         <div className="container mx-auto px-4 pt-20 pb-16 lg:pb-0 relative">
           <article className="grid grid-cols-1 lg:grid-cols-2 items-center gap-10">
-            {/* TEXTOS */}
             <div className="space-y-6 max-w-lg">
               <h1 className="text-4xl lg:text-5xl leading-tight">
                 Formação empreendedora <br />
@@ -64,6 +64,47 @@ export default function HomePage() {
           </article>
         </div>
       </section>
+      */}
+
+
+
+        <section className="relative bg-cover bg-center text-white" style={{ backgroundImage: "url('/Banner.png')" }}>
+          <div className="container mx-auto px-4 py-16">
+            <div className="space-y-6">
+              <h1 className="text-4xl lg:text-5xl font-semibold leading-tight">
+                Formação empreendedora <br />
+                para jovens e escolas <br />
+                com <b className="text-[#540075]">↝</b> impacto real
+              </h1>
+
+              <p className="text-lg">
+                Universos colaborativos para uma educação empreendedora!
+              </p>
+
+              <div className="flex items-center gap-4">
+                <a
+                  href="#"
+                  className="bg-[#761077] border-[3px] border-[#A22479] shadow-[0_0_0_4px_#F1477D] px-6 py-3 rounded-md font-semibold"
+                >
+                  Editais Abertos
+                </a>
+
+                <a
+                  href="#"
+                  className="bg-transparent border border-white px-6 py-3 rounded-md font-semibold"
+                >
+                  Saiba mais
+                </a>
+              </div>
+            </div>
+          </div>
+
+
+
+
+
+        </section>
+
 
       <div className="max-w-7xl mx-auto flex flex-col">
         <section className="bg-red-500">

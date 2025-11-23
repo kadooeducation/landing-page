416c155ebdc5d7cd112b26abd8adad600fe70ce9
jrmesquta7 <cleobetojunior1999@gmail.com>
2025-11-22 21:29:57 -0300

    feat: criando seção plataforma

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AccordionSection from "@/src/components/AccordionSection";
 
 export default function HomePage() {
   return (
@@ -191,9 +192,38 @@ export default function HomePage() {
 
         </section>
 
-        <section>
-          <span>seção de benoni</span>
+        <section className="w-full flex justify-center mt-16 mb-8">
+          <div className="inline-flex items-center gap-2 px-4 py-2 bg-[#E5D6EB] text-[#540075] rounded-md font-medium">
+            {/* Ícone de Lâmpada SVG */}
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              width="20" 
+              height="20" 
+              viewBox="0 0 24 24" 
+              fill="none" 
+              stroke="currentColor" 
+              strokeWidth="2" 
+              strokeLinecap="round" 
+              strokeLinejoin="round"
+            >
+              <path d="M15 14c.2-1 .7-1.7 1.5-2.5 1-1 1.5-2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5" />
+              <path d="M9 18h6" />
+              <path d="M10 22h4" />
+            </svg>
+            <span>Kadoo Academy</span>
+          </div>
         </section>
+
+        <section className=" pb-8 flex flex-col items-center px-4">
+          <h2 className="text-3xl md:text-5xl font-semibold text-center mb-4 text-[#333333] tracking-tight">
+           Plataforma com tudo que você precisa
+          </h2>
+          <p className="text-base md:text-lg text-center max-w-lg mx-auto text-[#5C5C5C] leading-relaxed">
+            Uma plataforma web cheia de funcionalidades onde toda a jornada de pré-aceleração acontece!
+          </p>
+        </section>
+
+        <AccordionSection />
 
       </div>
     </main>

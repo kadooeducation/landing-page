00fdf4fe1a0d0ed15ad83ac294f6cf540ebf5e8f
jrmesquta7 <cleobetojunior1999@gmail.com>
2025-11-23 14:52:25 -0300

    refactor: refatorando banner

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import PlataformSection from "@/src/components/PlataformSection"; 
 import ProblemSection from "@/src/components/ProblemSection";
 import PartnersSection from "@/src/components/PartnersSection";
+import BannerSection from "@/src/components/BannerSection";
 
 export default function HomePage() {
   
@@ -38,50 +39,7 @@ export default function HomePage() {
         </nav>
       </header>
 
-      <section className="relative overflow-hidden bg-gradient-to-r from-[#F1467D] to-[#FEB1CA] text-white">
-        <div className="container mx-auto px-4 pt-20 pb-16 lg:pb-0 relative">
-          <article className="grid grid-cols-1 lg:grid-cols-2 items-center gap-10">
-            <div className="space-y-6 max-w-lg">
-              <h1 className="text-4xl lg:text-5xl leading-tight">
-                Formação empreendedora <br />
-                para jovens e escolas <br />
-                com <b className="text-[#540075]">↝</b> impacto real
-              </h1>
-
-              <p className="text-lg opacity-90">
-                Universos colaborativos para uma educação empreendedora!
-              </p>
-
-              <div className="flex items-center gap-4">
-                <a
-                  href="#"
-                  className="bg-[#540075] px-6 py-3 rounded-md font-semibold text-white bg-[#761077] border-[3px] border-[#A22479] shadow-[0_0_0_4px_#F1477D]"
-                >
-                  Editais Abertos
-                </a>
-
-                <a
-                  href="#"
-                  className="bg-transparent border border-white text-white px-6 py-3 rounded-md font-semibold"
-                >
-                  Saiba mais
-                </a>
-              </div>
-            </div>
-
-            <div className="relative hidden lg:block h-[380px] w-full">
-              <Image
-                src="/people.png"
-                alt="Imagem de pessoas"
-                fill
-                className="object-cover object-center"
-                quality={100}
-                priority
-              />
-            </div>
-          </article>
-        </div>
-      </section>
+      <BannerSection />
 
       <div className="max-w-7xl mx-auto flex flex-col items-center">
 
